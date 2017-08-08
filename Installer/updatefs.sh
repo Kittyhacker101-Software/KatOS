8a5741eb4506fcd1a44be7bd657b0a67a7e27b3c
kittyhacker101 <kittyhacker101@protonmail.com>
2017-08-08 11:13:06 -0700

    Removed unneeded build steps

--- Installer/updatefs.sh
+++ Installer/updatefs.sh
@@ -16,17 +16,11 @@ find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpi
 cd ..
 rm -r rootfs
 
-tar -xvf syslinux.tar.xz
-cd $(ls -d syslinux-*)
-SYSLINUX=$(pwd)
-cd ..
-
 # Build boot partition data..
 mkdir isoimage
 cp ../Core/Kernel/kernelImage ./isoimage/kernel.xz
 cp rootfs.cpio.xz ./isoimage/rootfs.xz
 cd isoimage
-cp $SYSLINUX/bios/core/isolinux.bin .
 mkdir -p efi/boot
 cat << CEOF > ./efi/boot/startup.nsh
 echo -off
@@ -37,7 +31,6 @@ tar cfv - * | xz -9 -e --check=none > ../boot.tar.xz
 cd ..
 rm -r isoimage
 rm rootfs.cpio.xz
-rm -r $SYSLINUX
 
 # Move files into installer filesystem
 mv boot.iso InstallFS/root/boot.iso
