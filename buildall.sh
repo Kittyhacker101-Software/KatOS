39463f58ba90452aa64c77e83519da49ced05e71
kittyhacker101 <kittyhacker101@protonmail.com>
2017-08-10 15:13:52 -0700

    Minor fixes

--- buildall.sh
+++ buildall.sh
@@ -1,28 +1,16 @@
 #!/bin/sh
 
-
 cd Source
 sh download.sh
 cd ..
 
-
 cd Core
-
-cd Busybox
 sh build.sh
 cd ..
-
-cd Kernel
-sh build.sh
-cd ..
-
-cd ..
-
 
 cd Root
 sh build.sh
 cd ..
-
 
 cd Installer
 sh build.sh
