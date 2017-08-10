f71be891f3ac05b882db20182bf422cb057c8b7d
kittyhacker101 <kittyhacker101@protonmail.com>
2017-08-10 14:23:47 -0700

    Edit build process

--- Root/build.sh
+++ Root/build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 rm rootfs.tar.xz
 
+cp -v ../Core/Busybox/busybox_installed -r install/
 cp ../Source/alpine-keys-*.apk apk-keys.apk
 cp ../Source/apk-tools-static-*.apk apk-static.apk
 cp ../Source/musl-*.apk musl.apk
 cp ../Source/busybox-*.apk busybox.apk
-mkdir install
-cp ../Core/Busybox/busybox_installed/* -r -v install/
 cd install
 tar -xvf ../apk-keys.apk
 tar -xvf ../musl.apk
@@ -28,13 +27,6 @@ http://nl.alpinelinux.org/alpine/edge/testing/
 CEOF
 
 cd install
-strip --strip-all -v \
-  install/bin/* \
-  install/usr/bin/* \
-  install/sbin/* \
-  install/usr/sbin/* \
-  install/lib/* \
-  install/usr/lib/*
 tar cfv - * | xz -9 -e --check=none > ../rootfs.tar.xz
 cd ..
 rm -r install
