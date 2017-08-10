26f84ea3b2b16f66bc3addaf6ad3869fc065a373
kittyhacker101 <kittyhacker101@protonmail.com>
2017-08-10 14:54:43 -0700

    Minor changes

--- Installer/InstallFS/root/initi.sh
+++ Installer/InstallFS/root/initi.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+clear
+
 echo "Press enter to install KatOS. Press any other key to exit to the LiveCD."
 
-read -t 60 -n1 -s key
+read -n1 -s key
 
 if [ "$key" = "" ] ; then
   sh "/root/install.sh"
 else 
-  echo "Installer exited. Type /root/install.sh to start the installer within the LiveCD."
+  echo "Installer exited. Type /root/install.sh to start the installer again."
   cttyhack /bin/sh
 fi
