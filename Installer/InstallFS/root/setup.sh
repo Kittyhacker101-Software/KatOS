6e1020053ba8dd0eab88cc4e91535d372ea23a77
kittyhacker101 <kittyhacker101@protonmail.com>
2017-09-03 12:11:51 -0700

    Include Glibc by default

--- Installer/InstallFS/root/setup.sh
+++ Installer/InstallFS/root/setup.sh
@@ -2,11 +2,16 @@
 
 apk --root / --initdb add
 apk update
-apk add alpine-keys musl openssl busybox apk-tools
+apk add alpine-keys musl openssl busybox apk-tools ca-certificates
 apk cache clean
 
+# Glibc Compatibility is for Kittyhacker101 Software programs, which depend on Glibc
+wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.25-r0/glibc-2.25-r0.apk
+apk add glibc-2.25-r0.apk
+rm glibc-2.25-r0.apk
+
 clear
-#echo -e 'toor\ntoor\n' | passwd root
 passwd root
 
 clear
