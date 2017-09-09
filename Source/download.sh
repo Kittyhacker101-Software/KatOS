fdda58ed4f339390ddfa58de76d5ae85090604c0
kittyhacker101 <kittyhacker101@protonmail.com>
2017-09-08 22:47:05 -0700

    Update source URLs

--- Source/download.sh
+++ Source/download.sh
@@ -3,8 +3,8 @@ rm *.apk
 rm *.tar.xz
 rm *.tar.bz2
 
-wget https://www.busybox.net/downloads/busybox-1.27.1.tar.bz2
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.5.tar.xz
+wget https://www.busybox.net/downloads/busybox-1.27.2.tar.bz2
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.tar.xz
 wget https://www.kernel.org/pub/linux/utils/boot/syslinux/Testing/6.04/syslinux-6.04-pre1.tar.xz
 wget http://nl.alpinelinux.org/alpine/latest-stable/main/x86_64/alpine-keys-2.1-r1.apk
 wget http://nl.alpinelinux.org/alpine/latest-stable/main/x86_64/apk-tools-static-2.7.2-r0.apk
