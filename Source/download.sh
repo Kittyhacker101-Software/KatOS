825a94db0e3eebb4a539d0ae929583ac592203a8
kittyhacker101 <kittyhacker101@protonmail.com>
2017-09-16 09:53:09 -0700

    Update kernel URL

--- Source/download.sh
+++ Source/download.sh
@@ -4,7 +4,7 @@ rm *.tar.xz
 rm *.tar.bz2
 
 wget https://www.busybox.net/downloads/busybox-1.27.2.tar.bz2
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.2.tar.xz
 wget https://www.kernel.org/pub/linux/utils/boot/syslinux/Testing/6.04/syslinux-6.04-pre1.tar.xz
 wget http://nl.alpinelinux.org/alpine/latest-stable/main/x86_64/alpine-keys-2.1-r1.apk
 wget http://nl.alpinelinux.org/alpine/latest-stable/main/x86_64/apk-tools-static-2.7.2-r0.apk
