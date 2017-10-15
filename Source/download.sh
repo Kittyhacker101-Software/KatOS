2ac1765a6291f913b0c98c4204f7983cb07168f6
Kat <kittyhacker101@protonmail.com>
2017-10-15 12:46:53 -0700

    Update to latest kernel

--- Source/download.sh
+++ Source/download.sh
@@ -4,7 +4,7 @@ rm *.tar.xz
 rm *.tar.bz2
 
 wget https://www.busybox.net/downloads/busybox-1.27.2.tar.bz2
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.5.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.7.tar.xz
 wget https://www.kernel.org/pub/linux/utils/boot/syslinux/Testing/6.04/syslinux-6.04-pre1.tar.xz
 wget http://nl.alpinelinux.org/alpine/v3.6/main/x86_64/alpine-keys-2.1-r1.apk
 wget http://nl.alpinelinux.org/alpine/v3.6/main/x86_64/apk-tools-static-2.7.3-r0.apk
