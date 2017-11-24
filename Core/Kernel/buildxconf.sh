9b661618a64bbb77b1bfdf5fa38b72ce0b4c9c1e
Kat <kittyhacker101@protonmail.com>
2017-11-23 21:38:04 -0800

    Split kernel build scripts

--- Core/Kernel/buildxconf.sh
+++ Core/Kernel/buildxconf.sh
@@ -5,10 +5,10 @@ tar -xvf ../../Source/linux-*.tar.xz
 cd $(ls -d linux-*)
 make mrproper -j 32
 cp ../buildconfig .config
+make xconfig
 make \
   CFLAGS="-O3 -s -U_FORTIFY_SOURCE -pipe" \
   bzImage -j 32
 cp arch/x86_64/boot/bzImage \
   ../kernelImage
 cd ..
-rm -r linux-*/
