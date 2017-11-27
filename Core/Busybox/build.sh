e06076a26e2bc58cca2867c1ccf3c17a91d5f34d
Kat <kittyhacker101@protonmail.com>
2017-11-26 19:51:37 -0800

    Edited build priority

--- Core/Busybox/build.sh
+++ Core/Busybox/build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 rm -r busybox_installed
 
-tar -xvf ../../Source/busybox-*.tar.bz2
+chrt -b 0 nice -n 20 tar -xvf ../../Source/busybox-*.tar.bz2
 cd $(ls -d busybox-*)
-make distclean -j 32
-make defconfig -j 32
+chrt -b 0 nice -n 20 make distclean -j 32
+chrt -b 0 nice -n 20 make defconfig -j 32
 sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
-make \
+chrt -b 0 nice -n 20 make \
   EXTRA_CFLAGS="-Os -s -fno-stack-protector -fomit-frame-pointer -U_FORTIFY_SOURCE -pipe" \
   busybox -j 32
-make \
+chrt -b 0 nice -n 20 make \
   CONFIG_PREFIX="../busybox_installed" \
   install -j 32
 cd ..
