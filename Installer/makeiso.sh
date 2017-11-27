91dd195b830e0eac7e52129404b4c8d5b1a7da6c
Kat <kittyhacker101@protonmail.com>
2017-11-26 20:20:33 -0800

    Edited build priority

--- Installer/makeiso.sh
+++ Installer/makeiso.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 rm output.iso
 rm extinit.iso
-
+alias batch="chrt -b 0 nice -n 20"
 
 # Build installer image
 cp -v ../Core/Busybox/busybox_installed -r rootfs
 cd rootfs
-tar -xvf ../../Core/Basefs.tar.xz
+batch tar -xvf ../../Core/Basefs.tar.xz
 cp -v ../InstallFS/* -r .
-find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpio.xz
+batch find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpio.xz
 cd ..
 rm -r rootfs
 
-tar -xvf ../Source/syslinux-*.tar.xz
+batch tar -xvf ../Source/syslinux-*.tar.xz
 cd $(ls -d syslinux-*)
 SYSLINUX=$(pwd)
 cd ..
@@ -23,7 +23,7 @@ cp rootfs.cpio.xz ./isoimage/rootfs.xz
 cd isoimage
 cp $SYSLINUX/bios/core/isolinux.bin .
 cp $SYSLINUX/bios/com32/elflink/ldlinux/ldlinux.c32 .
-strip --strip-all --discard-all ldlinux.c32
+batch strip --strip-all --discard-all ldlinux.c32
 echo 'default kernel.xz initrd=rootfs.xz quiet' > ./syslinux.cfg
 mkdir -p efi/boot
 cat << CEOF > ./efi/boot/startup.nsh
@@ -33,7 +33,7 @@ echo "Loading KatOS LiveCD..."
 CEOF
 
 cd ..
-genisoimage \
+batch genisoimage \
   -J \
   -r \
   -o output.iso \
@@ -52,9 +52,9 @@ rm -r isoimage
 # Build external init
 cp -v ../Core/Busybox/busybox_installed -r rootfs
 cd rootfs
-tar -xvf ../../Core/InitExtfs.tar.xz
+batch tar -xvf ../../Core/InitExtfs.tar.xz
 cp ../InstallFS/root/boot.tar.xz root/boot.tar.xz
-find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpio.xz
+batch find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpio.xz
 cd ..
 rm -r rootfs
 
@@ -64,7 +64,7 @@ cp rootfs.cpio.xz ./isoimage/rootfs.xz
 cd isoimage
 cp $SYSLINUX/bios/core/isolinux.bin .
 cp $SYSLINUX/bios/com32/elflink/ldlinux/ldlinux.c32 .
-strip --strip-all --discard-all ldlinux.c32
+batch strip --strip-all --discard-all ldlinux.c32
 echo 'default kernel.xz initrd=rootfs.xz quiet' > ./syslinux.cfg
 mkdir -p efi/boot
 cat << CEOF > ./efi/boot/startup.nsh
@@ -74,7 +74,7 @@ echo "Loading KatOS External Init..."
 CEOF
 
 cd ..
-genisoimage \
+batch genisoimage \
   -J \
   -r \
   -o extinit.iso \
--- Installer/updatefs.sh
+++ Installer/updatefs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 rm InstallFS/root/boot.tar.xz
 rm InstallFS/root/rootfs.tar.xz
+alias batch="chrt -b 0 nice -n 20"
 
 # Build Initrd
 cp -v ../Core/Busybox/busybox_installed -r rootfs
 cd rootfs
-tar -xvf ../../Core/Initfs.tar.xz
-find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpio.xz
+batch tar -xvf ../../Core/Initfs.tar.xz
+batch find . | cpio -R root:root -H newc -o -v | xz -9 -e --check=none > ../rootfs.cpio.xz
 cd ..
 rm -r rootfs
 
@@ -21,7 +22,7 @@ echo -off
 echo "Loading KatOS..."
 \\kernel.xz initrd=\\rootfs.xz quiet
 CEOF
-tar cfv - * | xz -9 -e --check=none > ../InstallFS/root/boot.tar.xz
+batch tar cfv - * | xz -9 -e --check=none > ../InstallFS/root/boot.tar.xz
 cd ..
 rm -r isoimage
 rm rootfs.cpio.xz
